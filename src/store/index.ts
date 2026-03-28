41456bf92244f66a4307f00d67c8bbf98abad36f
Dimka06 <Dimitri.desfray@hotmail.com>
2026-03-28 10:00:57 +0100

    feat: add sidebar and command palette state to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,9 +3,17 @@ import { create } from "zustand";
 interface AppState {
   searchQuery: string;
   setSearchQuery: (q: string) => void;
+  sidebarOpen: boolean;
+  setSidebarOpen: (open: boolean) => void;
+  commandPaletteOpen: boolean;
+  setCommandPaletteOpen: (open: boolean) => void;
 }
 
 export const useAppStore = create<AppState>((set) => ({
   searchQuery: "",
   setSearchQuery: (q) => set({ searchQuery: q }),
+  sidebarOpen: true,
+  setSidebarOpen: (open) => set({ sidebarOpen: open }),
+  commandPaletteOpen: false,
+  setCommandPaletteOpen: (open) => set({ commandPaletteOpen: open }),
 }));
